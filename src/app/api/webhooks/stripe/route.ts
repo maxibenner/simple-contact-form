18936e68b5d36be483179a7b416fe6c44b9b4f0a
Maximilian Benner <maxibenner@gmail.com>
2025-05-21 17:57:49 -0400

    feat: added webhook protection

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -7,11 +7,28 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
 
 // Stripe endpoint to receive charge webhooks and update the teams balance accordingly
 export async function POST(request: Request) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let payloadData: any
+
+  const rawBody = await request.text()
+  const sig = request.headers.get('stripe-signature') ?? ''
+  const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SIGNING_SECRET || ''
+
   try {
-    const payloadData = await request.json()
+    payloadData = stripe.webhooks.constructEvent(rawBody, sig, stripeWebhookSecret)
+
+    if (!payloadData) return new Response('Invalid payload', { status: 400 })
+  } catch (_) {
+    return new Response(`Signature verification failed`, { status: 400 })
+  }
+
+  try {
+    // const payloadData = await request.json()
     const metadata = payloadData.data.object.metadata
 
-    // Payment method was attached successfully
+    // -------------------------------------------------
+    // Payment method attached
+    // -------------------------------------------------
     if (payloadData.type === 'payment_method.attached') {
       // Check if the team already has a payment method
       const existingPaymentMethod = await payload.find({
@@ -93,6 +110,9 @@ export async function POST(request: Request) {
       }
     }
 
+    // -------------------------------------------------
+    // Payment method detached
+    // -------------------------------------------------
     if (payloadData.type === 'payment_method.detached') {
       // Remove the payment method from db
       await payload.delete({
@@ -103,6 +123,59 @@ export async function POST(request: Request) {
           },
         },
       })
+    }
+
+    // -------------------------------------------------
+    // Charge succeeded
+    // -------------------------------------------------
+    if (payloadData.type === 'charge.succeeded') {
+      const amount = payloadData.data.object.amount
+      const autoRecharge = payloadData.data.object.metadata.autoRecharge
+      const teamId = payloadData.data.object.metadata.team
+      const futureUse = payloadData.data.object.metadata.futureUse
+
+      // Get current balance
+      const { balance, autoRecharge: autoRechargeExisting } = await payload.findByID({
+        collection: 'teams',
+        id: teamId,
+      })
+
+      // Update the team's balance
+      await payload.update({
+        collection: 'teams',
+        id: teamId,
+        data: {
+          balance: balance !== undefined || balance !== null ? balance + amount : 0,
+          autoRecharge:
+            autoRechargeExisting || (futureUse && autoRecharge === 'true') ? true : false,
+        },
+      })
+
+      // Check if customer has a billing address in stripe
+      const customer = await stripe.customers.retrieve(payloadData.data.object.customer, {
+        expand: ['default_source'],
+      })
+
+      // Parse address from payload
+      const newAddress = metadata.address && JSON.parse(metadata.address)
+
+      if (newAddress) {
+        await stripe.customers
+          .update(customer.id, {
+            name: newAddress.value.name,
+            address: {
+              city: newAddress.value.address.city,
+              country: newAddress.value.address.country,
+              line1: newAddress.value.address.line1,
+              line2: newAddress.value.address.line2,
+              postal_code: newAddress.value.address.postal_code,
+              state: newAddress.value.address.state,
+            },
+          })
+          .catch((error) => {
+            console.error('Error updating customer address:', error)
+          })
+      }
     }
 
     return new Response('Webhook received', { status: 200 })
